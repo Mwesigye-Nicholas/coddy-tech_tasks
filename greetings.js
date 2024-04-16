e1a3c3c6300efbafe7ca6b05721ada270deeb5ee
Mwesigye Nicholas <nicholastoshack123451111@gmail.com>
2024-04-16 13:13:25 -0400

    greetings

---
+++ greetings.js
@@ -0,0 +1,12 @@
+/**
+ * greetings - consoles customised
+ * name greetings
+ *
+ * @name: customised name for the greetings
+ *
+ * Return: greetings message
+ */
+
+const greetings = name => `Hello, ${name}`;
+console.log(greetings("Mwesigye Nicholas"));
+console.log(greetings("JavaScript"));
